c0e66def5e8c9eac9d21b9dcd146ff402e937095
Luis_Filipe <l.fpagee@gmail.com>
2019-09-19 20:30:58 -0300

    Primeiro Commit

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core' ;
+
+
+@Component({
+
+    selector: 'app-topo',
+    templateUrl: './topo.component.html',
+    styleUrls: ['./topo.component.css']
+    //template: '<p>Podemos referenciar o template inline de nosso atributo template , diretamente da sua intanialção no meta dado do component</p>'
+    //style : [ .exemplo {color : red}]  referenciando inline o stilo, na aplicação instanciar a class="exemplo " no arquivo html 
+})
+export default class TopoComponent {}
